8f0d0c986ca9507893edcd62f4e018b00e574ec8
Jake Swenson <jswenson@godaddy.com>
2016-04-22 21:02:41 -0700

    adding settings file

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ openssl aes-256-cbc -K ${GPG_PRIVATE_KEY_ENCRYPTION_KEY} -iv ${GPG_PRIVATE_KEY_E
 
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Deploying release version for tag '${TRAVIS_TAG}'"
-    mvn clean deploy --settings settings.xml -DskipTests -P release -Drevision=''
+    mvn clean deploy --settings "${CURRENT_DIR}/settings.xml" -DskipTests -P release -Drevision=''
     exit $?
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Deploying snapshot version on branch '${TRAVIS_BRANCH}'"
-    mvn clean deploy --settings settings.xml -DskipTests
+    mvn clean deploy --settings "${CURRENT_DIR}/settings.xml" -DskipTests
     exit $?
 fi
