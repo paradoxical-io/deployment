56b1499e73c218aeb3c51016255782e29c9a226d
Jake Swenson <jake@jakeswenson.com>
2016-04-22 20:54:09 -0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ if [ -z "$GPG_PRIVATE_KEY_ENCRYPTION_IV" ]; then
     exit 0
 fi
 
+CURRENT_DIR=`dirname $0`
+
+openssl aes-256-cbc -K ${GPG_PRIVATE_KEY_ENCRYPTION_KEY} -iv ${GPG_PRIVATE_KEY_ENCRYPTION_IV} \
+  -in "${CURRENT_DIR}/gpg/paradoxical-io-private.gpg.enc" \
+  -out "${CURRENT_DIR}/gpg/paradoxical-io-private.gpg" -d
+
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Deploying release version for tag '${TRAVIS_TAG}'"
     mvn clean deploy --settings settings.xml -DskipTests -P release -Drevision=''
