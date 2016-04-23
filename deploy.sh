628b70037f66b58cc9a226fb8571f40be2f0023a
Jake Swenson <jake@jakeswenson.com>
2016-04-22 21:10:34 -0700

    updating paths

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,18 @@ if [ -z "$GPG_PRIVATE_KEY_ENCRYPTION_IV" ]; then
     exit 0
 fi
 
-CURRENT_DIR=`dirname $0`
+SCRIPT_DIR=`dirname $0`
 
 openssl aes-256-cbc -K ${GPG_PRIVATE_KEY_ENCRYPTION_KEY} -iv ${GPG_PRIVATE_KEY_ENCRYPTION_IV} \
-  -in "${CURRENT_DIR}/gpg/paradoxical-io-private.gpg.enc" \
-  -out "${CURRENT_DIR}/gpg/paradoxical-io-private.gpg" -d
+  -in "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg.enc" \
+  -out "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg" -d
 
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Deploying release version for tag '${TRAVIS_TAG}'"
-    mvn clean deploy --settings "${CURRENT_DIR}/settings.xml" -DskipTests -P release -Drevision=''
+    mvn clean deploy --settings "${SCRIPT_DIR}/settings.xml" -DskipTests -P release -Drevision=''
     exit $?
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Deploying snapshot version on branch '${TRAVIS_BRANCH}'"
-    mvn clean deploy --settings "${CURRENT_DIR}/settings.xml" -DskipTests
+    mvn clean deploy --settings "${SCRIPT_DIR}/settings.xml" -DskipTests
     exit $?
 fi
