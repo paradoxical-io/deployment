bdea24d63228f0caaba48459b1fe4fd9819eef9e
Jake Swenson <jake@jakeswenson.com>
2016-12-04 11:40:14 -0800

    improve the script a bit; alowing build branch to be specified; also allowing version to be set as the revision vara

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,16 @@
 SCRIPT_DIR=`dirname $0`
 
 echo "Deployment script running..."
+
+
+if [ -z "${BUILD_BRANCH}" ]; then
+    BUILD_BRANCH='master'
+fi
+
+if [ -z "${USE_TAG_VERSION}" ]; then
+    USE_TAG_VERSION='false'
+fi
+
 echo "=== Build parameters ===
     TRAVIS_TAG=${TRAVIS_TAG}
     TRAVIS_BRANCH=${TRAVIS_BRANCH}
@@ -17,7 +27,8 @@ echo "=== Build parameters ===
     TRAVIS_SECURE_ENV_VARS=${TRAVIS_SECURE_ENV_VARS}
     SCRIPT_DIR=${SCRIPT_DIR}
     CONTINUOUS_INTEGRATION=${CONTINUOUS_INTEGRATION}
-
+    BUILD_BRANCH=${BUILD_BRANCH}
+    USE_TAG_VERSION=${USE_TAG_VERSION}
 "
 
 if [ "$TRAVIS_SECURE_ENV_VARS" != "true" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
@@ -26,20 +37,34 @@ if [ "$TRAVIS_SECURE_ENV_VARS" != "true" -o "$TRAVIS_PULL_REQUEST" != "false" ];
     exit 0
 fi
 
-echo "Decrypting gpg key..."
+_decrypt_gpg() {
+    echo "Decrypting gpg key..."
 
-openssl aes-256-cbc -K ${GPG_PRIVATE_KEY_ENCRYPTION_KEY} -iv ${GPG_PRIVATE_KEY_ENCRYPTION_IV} \
-  -in "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg.enc" \
-  -out "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg" -d
+    openssl aes-256-cbc -K "${GPG_PRIVATE_KEY_ENCRYPTION_KEY}" -iv "${GPG_PRIVATE_KEY_ENCRYPTION_IV}" \
+      -in "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg.enc" \
+      -out "${SCRIPT_DIR}/gpg/paradoxical-io-private.gpg" -d
 
-echo "DONE decrypting gpg key!"
+    echo "DONE decrypting gpg key!"
+}
 
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Deploying release version for tag '${TRAVIS_TAG}'"
-    mvn clean deploy --settings "${SCRIPT_DIR}/settings.xml" -DskipTests -P release -Drevision='' $@
+
+    REVISION=''
+
+    if [ "${USE_TAG_VERSION}" = "true" ]; then
+        REVISION="$TRAVIS_TAG"
+    fi
+
+    _decrypt_gpg
+
+    mvn clean deploy --settings "${SCRIPT_DIR}/settings.xml" -DskipTests -P release -Drevision="$REVISION" $@
     exit $?
-elif [ "$TRAVIS_BRANCH" = "master" ]; then
+elif [ "$TRAVIS_BRANCH" = "${BUILD_BRANCH}" ]; then
     echo "Deploying snapshot version on branch '${TRAVIS_BRANCH}'"
+
+    _decrypt_gpg
+
     mvn clean deploy --settings "${SCRIPT_DIR}/settings.xml" -DskipTests -P snapshot $@
     exit $?
 else
